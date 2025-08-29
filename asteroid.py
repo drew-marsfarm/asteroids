37dc803a26b9af3e0b797798904987a78d5c4e6f
Drew Thomas <drew@marsfarm.io>
2025-08-29 00:15:13 -0500

    Added asteroids

---
+++ asteroid.py
@@ -0,0 +1,12 @@
+import pygame
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw():
+        pygame.draw.circle(super().position, super().radius, width=2)
+
+    def update():
+        (super().velocity * dt) + super().position
