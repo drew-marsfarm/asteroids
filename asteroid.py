fb582f8c34043d7a27c8150173792acc7ca3802f
Drew Thomas <drew@marsfarm.io>
2025-08-29 00:24:45 -0500

    bug fix

--- asteroid.py
+++ asteroid.py
@@ -4,9 +4,11 @@ from circleshape import *
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.position = pygame.Vector2(x, y)
+        self.radius = radius
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", super().position, super().radius, width=2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
 
     def update(dt):
         (super().velocity * dt) + super().position
